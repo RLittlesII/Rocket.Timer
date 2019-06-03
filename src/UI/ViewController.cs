f690e20d006d12d75dec39434bc9b746aab3816c
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-06-03 18:32:59 -0500

    Added ReactiveUI
    Added ViewControllerBase

--- src/UI/ViewController.cs
+++ src/UI/ViewController.cs
@@ -2,13 +2,16 @@
 
 using AppKit;
 using Foundation;
+using ReactiveUI;
 
 namespace UI
 {
-    public partial class ViewController : NSViewController
+    public partial class ViewController : ViewControllerBase<MainViewModel>
     {
-        public ViewController(IntPtr handle) : base(handle)
+        public ViewController(IntPtr handle)
+            : base(handle)
         {
+            ViewModel = new MainViewModel();
         }
 
         public override void ViewDidLoad()
@@ -16,6 +19,10 @@ namespace UI
             base.ViewDidLoad();
 
             // Do any additional setup after loading the view.
+        }
+
+        protected override void BindControls()
+        {
         }
 
         public override NSObject RepresentedObject
@@ -29,6 +36,17 @@ namespace UI
                 base.RepresentedObject = value;
                 // Update the view, if already loaded.
             }
+        }
+    }
+
+    public class MainViewModel : ReactiveObject
+    {
+        private string _timer;
+
+        public string Timer
+        {
+            get => _timer;
+            set => this.RaiseAndSetIfChanged(ref _timer, value);
         }
     }
 }
---
+++ src/UI/ViewControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ReactiveUI;
+
+namespace UI
+{
+    public abstract class ViewControllerBase<T> : ReactiveViewController<T>
+        where T : class
+    {
+        protected ViewControllerBase(IntPtr intPtr)
+            : base(intPtr)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            BindControls();
+        }
+
+        protected abstract void BindControls();
+    }
+}
